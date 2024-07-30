ac39b63ca8a7e531fc17bac98f0d009c08793097
UmutHarunJS <harun123ors@gmail.com>
2024-07-30 16:00:53 +0300

    signalR added and tested

---
+++ RealEstateWebApi/Hubs/SignalRHub.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace RealEstateWebApi.Hubs
+{
+    public class SignalRHub : Hub
+    {
+    }
+}
--- RealEstateWebApi/Program.cs
+++ RealEstateWebApi/Program.cs
@@ -1,3 +1,4 @@
+using RealEstateWebApi.Hubs;
 using RealEstateWebApi.Models.DapperContext;
 using RealEstateWebApi.Repositories.BottomGridRepository;
 using RealEstateWebApi.Repositories.CategoryRepository;
@@ -27,6 +28,17 @@ builder.Services.AddTransient<IStatisticRepository, StatisticRepository>();
 builder.Services.AddTransient<IContactRepository, ContactRepository>(); 
 builder.Services.AddTransient<IToDoListRepository, ToDoListRepository>(); 
 
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyHeader()
+            .AllowAnyMethod()
+            .SetIsOriginAllowed((host) => true)
+            .AllowCredentials();
+    });
+});
+builder.Services.AddSignalR();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -41,10 +53,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHub<SignalRHub>("/signalrhub");
 
 app.Run();
