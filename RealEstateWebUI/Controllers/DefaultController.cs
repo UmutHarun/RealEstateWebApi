37de74cf0451d24b1103d6d86713b1b73d1f9817
UmutHarunJS <harun123ors@gmail.com>
2024-08-14 23:31:58 +0300

    search ui added

--- RealEstateWebUI/Controllers/DefaultController.cs
+++ RealEstateWebUI/Controllers/DefaultController.cs
@@ -1,25 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using NuGet.Configuration;
+using RealEstateWebUI.Dtos.CategoryDtos;
+using System.Net.Http;
 
 namespace RealEstateWebUI.Controllers
 {
     public class DefaultController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+		private readonly IHttpClientFactory _httpClientFactory;
+		private readonly ApiSettings _apiSettings;
+		public DefaultController(IHttpClientFactory httpClientFactory, IOptions<ApiSettings> apiSettings)
+		{
+			_httpClientFactory = httpClientFactory;
+			_apiSettings = apiSettings.Value;
+		}
 
-        [HttpGet]
-        public PartialViewResult PartialSearch()
-        {
-            return PartialView();
-        }
+		public async Task<IActionResult> Index()
+		{
+			var client = _httpClientFactory.CreateClient();
+			client.BaseAddress = new Uri(_apiSettings.BaseUrl);
+			var responseMessage = await client.GetAsync("Categories");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
 
-        [HttpPost]
-        public IActionResult PartialSearch(string p)
+		public async Task<PartialViewResult> PartialSearch()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7232/api/Categories");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+				return PartialView(values);
+			}
+			return PartialView();
+		}
+
+		[HttpPost]
+        public IActionResult PartialSearch(string searchKeyValue, int propertyCategoryId, string city)
         {
-            TempData["word"] = p;
-            return RedirectToAction("PropertyListWithSearch","Property");
+			TempData["searchKeyValue"] = searchKeyValue;
+			TempData["propertyCategoryId"] = propertyCategoryId;
+			TempData["city"] = city;
+			return RedirectToAction("PropertyListWithSearch","Property");
         }
     }
 }
